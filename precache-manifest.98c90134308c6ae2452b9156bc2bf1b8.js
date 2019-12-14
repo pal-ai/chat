4788d944ebaec98c1ee60488fd96b6eac9f8697f
Anish Thite <anishthite@gmail.com>
2019-12-14 01:19:47 -0500

    Updates

---
+++ precache-manifest.98c90134308c6ae2452b9156bc2bf1b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f00d2cbcc957ede3828a7eb6041302",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "b7a0d1fb4b2a1ec513cb",
+    "url": "/chat/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "958f958d106efa6d4ca7",
+    "url": "/chat/static/js/2.90d68b05.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/chat/static/js/2.90d68b05.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7a0d1fb4b2a1ec513cb",
+    "url": "/chat/static/js/main.1d4b19a4.chunk.js"
+  },
+  {
+    "revision": "de0a1e66857097ad602a",
+    "url": "/chat/static/js/runtime-main.6ea358fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chat/static/media/logo.5d5d9eef.svg"
+  }
+]);
